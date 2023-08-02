49e28bcd937f6d7bc8242ce9c8c5a0cb8a80338c
david <david@augerhandle.net>
2023-08-02 08:06:04 -0500

    consistent title styling for single page

--- single.php
+++ single.php
@@ -6,7 +6,7 @@
 <?php the_post(); ?>
 
 			<div id="post-<?php the_ID(); ?>" class="<?php veryplaintxt_post_class(); ?>">
-				<h2 class="entry-title"><a href="<?php get_permalink(); ?>" rel="bookmark" style="text-decoration:underline"><?php the_title(); ?></a></h2>
+				<h2 class="entry-title"><a href="<?php get_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 				<div class="dah-entry-subtitle">
 					<span class="dah-date"><?php unset($previousday); printf(__('%1$s', 'veryplaintxt'), the_date('D, j M Y, h:i A (e)', false)) ?></span>
 					<span class="dah-license-badge" title="license info"><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a></span>
