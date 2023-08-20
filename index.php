3d19ace0f7013392aa7d0c42d71de3db17ccf547
david <david@augerhandle.net>
2023-08-19 22:55:45 -0500

    add explicity copyright notices

--- index.php
+++ index.php
@@ -25,6 +25,20 @@
 				<div class="nav-next"><?php previous_posts_link(__('NEWER&gt;', 'veryplaintxt')) ?></div>
 			</div>
 
+			<p class="dah-license-callout" xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/">
+&copy; 
+	<time datetime="<?php printf(__('%1$s', 'veryplaintxt'), the_date('D, j M Y, h:i A (e)', false)) ?>"><?php echo date('Y'); ?></time>
+	<a property="dct:title" 
+		rel="cc:attributionURL" 
+		href="<?php echo site_url('','https'); ?>"><?php bloginfo('name') ?></a> 
+	by 
+	<span property="cc:attributionName">David Hasan</span> 
+	is licensed under a 
+	<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+		Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+	</a>
+</p> 
+
 		</div><!-- #content .hfeed -->
 	</div><!-- #container -->
 
--- single.php
+++ single.php
@@ -18,5 +18,21 @@
 
 		</div><!-- #content .hfeed -->
 	</div><!-- #container -->
+
+<p class="dah-license-callout" xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/">
+&copy; 
+	<time datetime="<?php printf(__('%1$s', 'veryplaintxt'), the_date('D, j M Y, h:i A (e)', false)) ?>"><?php echo date('Y'); ?></time>
+	<a property="dct:title" 
+		rel="cc:attributionURL" 
+		href="<?php echo site_url('','https'); ?>"><?php bloginfo('name') ?></a> 
+	by 
+	<span property="cc:attributionName">David Hasan</span> 
+	is licensed under a 
+	<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+		Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+	</a>
+</p> 
+
+
 <?php get_sidebar() ?>
 <?php get_footer() ?>
